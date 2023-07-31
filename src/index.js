38a4ef0530c03660c3ac29b897ba51892b465863
Abid Ali Mohammed <abidali.mohammed@ovaledge.com>
2023-07-31 23:08:04 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function BookList(){
+  return <div className='booklist'>
+            <Book/> 
+            <Book/> 
+            <Book/> 
+            <Book/> 
+        </div>
+}
+const Author = 'Abid Ali';
+const Book = () =>{
+  let title = "Intresting Facts for Curious minds"
+  return <article className="book">
+          <img src="./images/img.jpeg" alt="Intresting Facts for Curious minds"/>
+          <h2>{title}</h2>
+          <h4>{Author.toUpperCase()}</h4>
+          {/* <p>{let x=6;}</p> */}
+          <p>{6+6}</p>
+        </article>
+}   
+
+
+const Message = () => {
+  return <p>This is the Message!</p>;
+}
+
+
+ReactDOM.render(
+  <BookList/>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
